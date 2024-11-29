269a857c24505466d6cc0e6491c12ed8349c5f24
Yeabsira Yohannes <yeabsira.yohannes@aastustudent.edu.et>
2024-11-29 22:17:12 +0300

    BMI_calculator.cpp

--- ETS1574-Yeabsira-Yohannes/coding.cpp
+++ ETS1574-Yeabsira-Yohannes/coding.cpp
@@ -1,1 +1,32 @@
 BMI-calculator
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double weight, height, bmi;
+    cout<<"enter the weight:";
+    cin>>weight;
+
+    cout<<"enter the height:";
+    cin>>height;
+
+    bmi=weight/(height*height);
+    cout<< "your bmi is:"<<bmi<<endl;
+
+    if(bmi<18.5) {
+        cout<<"underweight";
+
+    } else if(bmi>=18.5 && bmi<24.9) {
+        cout<<"normal weight";
+
+    } else if(bmi>=25 && bmi<29.9) {
+        cout<<"overweight";
+
+    } else {
+        cout<<"obesity";
+    }
+
+    return 0;
+
+}
