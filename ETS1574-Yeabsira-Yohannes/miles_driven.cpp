71db00dc6e41ee2792ef4106259b003eca56bee4
Yeabsira Yohannes <yeabsira.yohannes@aastustudent.edu.et>
2024-11-30 10:19:45 +0300

    miles_driven.cpp

--- ETS1574-Yeabsira-Yohannes/miles_driven.cpp
+++ ETS1574-Yeabsira-Yohannes/miles_driven.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+  
+ float gallon_capacity, milesPerGallon, distance;
+ cout << "input the capacity in gallons of your fuel tank: ";
+ cin >> gallon_capacity;
+ cout << "input how many miles the car can travel per gallon of gas: ";
+ cin >> milesPerGallon;
+ distance = gallon_capacity * milesPerGallon;
+ cout << "The car can travel " << distance << " miles without refueling." << endl;
 
+    return 0;
+}
