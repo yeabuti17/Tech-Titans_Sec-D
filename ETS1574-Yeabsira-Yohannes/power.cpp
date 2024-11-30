a619391181b63e00b290b793d2bde4ab3af0e459
Yeabsira Yohannes <yeabsira.yohannes@aastustudent.edu.et>
2024-11-30 10:42:24 +0300

    Update power.cpp

--- ETS1574-Yeabsira-Yohannes/power.cpp
+++ ETS1574-Yeabsira-Yohannes/power.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int x,y, product = 1;
+ cout << "enter the base number: ";
+ cin >> x;
+ cout << "enter the power number: ";
+ cin >> y;
+ if (y < 0) {
+        cout << "Error: The power number must be a non-negative integer." << endl;
+        return 1; 
+    }
+  for (int i = 1; i <= y; i++) {
+     product *= x;
+ }
+ cout << "the result is " << product;
 
+    return 0;
+}
